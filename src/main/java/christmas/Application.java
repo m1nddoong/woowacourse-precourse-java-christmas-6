7fda7eae08472136ccc9631797784622f15a1d15
MIN SEON IK <alstjsdlr0321@korea.ac.kr>
2023-11-14 18:11:29 +0900

    feat : 증정 메뉴에 대한 조건문 코드를 포함한 presentationMenu 메서드 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -9,12 +9,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
-        inputView.readDate();
+        int date = inputView.readDate();
         List<MenuItem> myorderList = inputView.readMenu(); // 내가 주문한 메뉴 목록
+        System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
 
         OutputView outputView = new OutputView();
         outputView.printMenu(myorderList);
-        outputView.MoneyBeforeDiscount(myorderList);
+        int sum = outputView.MoneyBeforeDiscount(myorderList);
+        sum = outputView.presentationMenu(myorderList, sum);
     }
 
 
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class OutputView {
     public void printMenu(List<MenuItem> myorderList) {
+        System.out.println();
         System.out.println("<주문 메뉴>");
         for (MenuItem menuItem : myorderList) {
             System.out.println(menuItem.getMenuName() + " " + menuItem.getQuantity() + "개");
@@ -12,14 +13,28 @@ public class OutputView {
         }
     }
 
-    public void MoneyBeforeDiscount(List<MenuItem> myorderList) {
+    public int MoneyBeforeDiscount(List<MenuItem> myorderList) {
         int sum = 0;
 
         System.out.println();
         System.out.println("<할인 전 총주문 금액>");
         for (MenuItem menuItem : myorderList) {
-            sum += menuItem.getPrice();
+            sum = sum + menuItem.getPrice() * menuItem.getQuantity();
         }
         System.out.println(sum + " 원");
+        return sum;
     }
+
+    public int presentationMenu(List<MenuItem> myorderList, int sum) {
+        System.out.println();
+        System.out.println("<증정 메뉴>");
+
+        if (sum >= 120000) {
+            System.out.println("샴페인 1개");
+            sum = sum - 25000;
+        }
+        return sum;
+    }
+
 }
+
